dda65e5b544b6fa745449791779bf09504b7e591
Clinton Contreras <ClintonC@zoovy.com>
2013-05-20 15:59:13 -0700

    modified auto-top scroll feature again

--- app-quickstart.js
+++ app-quickstart.js
@@ -881,8 +881,7 @@ for legacy browsers. That means old browsers will use the anchor to retain 'back
 				switch(pageType)	{
 
 					case 'product':
-	//add item to recently viewed list IF it is not already in the list.	
-						infoObj.performJumpToTop = false;			
+	//add item to recently viewed list IF it is not already in the list.				
 						if($.inArray(infoObj.pid,app.ext.myRIA.vars.session.recentlyViewedItems) < 0)	{
 							app.ext.myRIA.vars.session.recentlyViewedItems.unshift(infoObj.pid);
 							}
